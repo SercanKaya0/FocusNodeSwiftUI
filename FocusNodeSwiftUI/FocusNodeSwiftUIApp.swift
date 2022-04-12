99b781a9415488cb16ad0c9ab0caa33a025fefee
Sercan KAYA <sercan.kaya@icloud.com>
2022-04-12 21:43:23 +0300

    Initial Commit

---
+++ FocusNodeSwiftUI/FocusNodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusNodeSwiftUIApp.swift
+//  FocusNodeSwiftUI
+//
+//  Created by Sercan KAYA on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FocusNodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
